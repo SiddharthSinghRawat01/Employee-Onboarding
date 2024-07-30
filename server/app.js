485a2e3a7047c2d300988a5bf1da4df7bd2ab94c
SiddharthSinghRawat01 <siddharthsinghrawat01@gmail.com>
2024-07-30 20:42:28 +0530

    backend complete

--- server/app.js
+++ server/app.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
+require('dotenv').config();
 const db = require('./config/dbConnection')
 const bodyParser = require('body-parser');
-const PORT = 5001;
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5001;
 
 app.use(bodyParser.json())
-
+app.use(cors());
+app.use((req, res, next) => {
+    res.setTimeout(600000, () => { // 10 minutes
+        console.log('Request has timed out.');
+        res.sendStatus(408); // Request Timeout
+    });
+    next();
+});
 app.use('/api', require('./src/routes'));
 
-http.listen(PORT,(err)=>{
-    err ? 
-    console.log(`connection failed at port: ${PORT}`)
-    :
-    console.log(`connction successful port: ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
+app.listen(PORT, (err) => {
+    console.log(PORT);
+    err ?
+        console.log(`connection failed at port: ${PORT}`)
+        :
+        console.log(`connction successful port: ${PORT}`);
+    console.log(PORT);
 })
+
+module.exports = app;
